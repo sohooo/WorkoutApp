ec535e7ba17240cbdcb2f418cb301aacb76ae724
sohooo <sohooo@intothespirit.com>
2011-05-13 12:21:12 +0200

    list workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,9 @@
 class WorkoutsController < ApplicationController
+  def index
+    @workouts = Workout.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.rss
+    end
+  end
 end
