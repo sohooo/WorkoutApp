8d76ac2f6a114a864e105a8dae3c67005d519d2a
sohooo <sohooo@intothespirit.com>
2011-05-18 08:55:27 +0200

    happy with prototype styling now

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -1,6 +1,6 @@
 module WorkoutsHelper
   def workout_counter
-    pluralize(Workout.count, "workout")
+    Workout.count || 0
   end
 
   def to_kg(weight)
@@ -13,11 +13,34 @@ module WorkoutsHelper
     end
   end
 
+  # def workout_count(workouts)
+  #   content_tag :div, :class => "workout_count" do
+  #     if workouts.any?
+  #       pluralize(workouts.size, "workout") + " total"
+  #     else
+  #       "no workouts performed"
+  #     end
+  #   end
+  # end
+
+  def last_workout(workouts)
+    if workouts.any?
+      latest = Workout.order("date DESC").first
+      days_ago_in_words(latest.date)
+    else
+      "soon"
+    end
+  end
+
   def days_ago_in_words(date, include_seconds = false)
     to_time   = Time.now
     from_time = date.beginning_of_day
-    days_ago  = ((to_time - from_time)/1.day).abs.round
-    ["today", "yesterday"][days_ago] || 
+    days_ago  = ((to_time - from_time)/1.day).abs.round + 1
+    [nil, "today", "yesterday"][days_ago] || 
       distance_of_time_in_words(from_time, to_time, include_seconds) + " ago"
   end
+end
+
+def with_weight?(exercise)
+  !!exercise.weight
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,5 @@
 class Workout < ActiveRecord::Base
   has_many :exercises, :dependent => :delete_all
+
+  default_scope order('date desc')
 end
